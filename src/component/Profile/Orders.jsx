95338a5a4891294148cba7f3cdb206dbb3bedc06
Liru Zhao <zlrhhj@hotmail.com>
2025-04-16 10:16:11 -0700

    implemented the Orders components

---
+++ src/component/Profile/Orders.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import OrderCard from './OrderCard'; // Adjust the path based on the actual location of OrderCard
+
+
+const Orders = () => {
+    return (
+        <div className='flex flex-col items-center '>
+            <h1 className='text-xl text-center py-7 font-semibold'>My Orders</h1>
+            <div className='space-y-5 w-full lg:w-1/2' >
+            {
+                [1,1,1].map((item) => <OrderCard/>)
+            }
+            </div>
+        </div>
+    );
+};
+
+export default Orders;
