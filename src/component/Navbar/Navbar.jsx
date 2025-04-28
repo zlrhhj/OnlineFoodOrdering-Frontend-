ca5dccadcb929ac1eba38ebb36b78bdd4594560a
Liru Zhao <zlrhhj@hotmail.com>
2025-04-27 21:09:52 -0700

    modify the Avatar into Person Icon when the user does not login

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { IconButton, Avatar, Badge } from '@mui/material'
+import { IconButton, Avatar, Badge, Box } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
 import { pink } from '@mui/material/colors';
+import Person from '@mui/icons-material/Person';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import './Navbar.css';
+import { useNavigate } from 'react-router-dom';
 export const Navbar = () => {
+  const navigate = useNavigate();
   return (
-    <div className = 'px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between'>
+    <Box className = 'px-5 sticky top-0 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between'>
 
       <div className = 'lg:mr-10 cursor-pointer flex items-center space-x-4'>
         <li className="logo font-semibold text-gray-300 text-2xl">
@@ -22,10 +25,18 @@ export const Navbar = () => {
           </IconButton>
         </div>
         <div className=''>
-
-          <Avatar sx={{bgcolor:'white', color:pink.A400}}>
+        {
+          false
+          ?
+          <Avatar sx={{bgcolor:'white', color:pink.A400} }>
             C
           </Avatar>
+          :
+          <IconButton onClick={() => navigate('/account/login')}>
+            <Person>
+            </Person>
+          </IconButton>
+        }
         </div>
         <div>
           <IconButton>
@@ -36,7 +47,7 @@ export const Navbar = () => {
           </IconButton>
         </div>
       </div>
-    </div>
+    </Box>
 
   )
 }
